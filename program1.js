e7dca192cd9d47790a410f99fe664e217c61b685
VarshaGuduguntla <guduguntla.varsha@gmail.com>
2024-11-11 15:18:17 +0530

    changes done

--- program1.js
+++ program1.js
@@ -3,9 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
-};
 
-module.exports = { isValid };
+    
+       const stack = [];
+       const matchingParentheses = {
+           ')': '(',
+           '}': '{',
+           ']': '['
+       };
+   
+       for (let char of s) {
+           if (['(', '{', '['].includes(char)) {
+               // Push opening brackets to the stack
+               stack.push(char);
+           } else if ([')', '}', ']'].includes(char)) {
+               // Check if the stack is not empty and top element matches
+               if (stack.length === 0 || stack.pop() !== matchingParentheses[char]) {
+                   return false;
+               }
+           }
+       }
+       // If the stack is empty, all parentheses were matched correctly
+       return stack.length === 0;
+   };
+   
+   module.exports = { isValid };
+   
+
+
 
 
--- program2.js
+++ program2.js
@@ -3,8 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanToValue = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const current = romanToValue[s[i]];
+        const next = romanToValue[s[i + 1]];
+
+        // If the current value is less than the next, subtract it
+        if (next && current < next) {
+            total -= current;
+        } else {
+            // Otherwise, add it
+            total += current;
+        }
+    }
+
+    return total;
 };
+
 
 
 module.exports={romanToInt}
